09190d03542ac39ee870dde13d4b53767a5c1c74
giacomobranchi <branchigiacomo28@gmail.com>
2023-08-30 11:24:10 +0200

    refactoring

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,12 +2,6 @@
 L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.
 */
-let num = [];
-let num1 = prompt('inserisci un numero');
-let num2 = prompt('inserisci un altro numero');
-console.log(num1, num2);
-num.push(num1)
-const result = document.querySelector("div")
 // if (num1 > num2) {
 //     document.querySelector(".num").innerHTML = num1;
 // } else if (num2 > num1) {
@@ -15,35 +9,39 @@ const result = document.querySelector("div")
 // } else {
 //     document.querySelector(".num").innerHTML = num1;
 // }
+// let num1 = prompt('inserisci un numero');
+// let num2 = prompt('inserisci un altro numero');
+// console.log(num1, num2);
+// num.push(num1)
+// const result = document.querySelector("div")
 
 
 
+const num = [];
 let i = 0;
-while (i < num.length) {
-    const element = num[i];
+while (i < 2) {
+    const userNum = Number(prompt(`Scegli un numero`));
 
-    if (isNaN(element)) {
-        result.innerHTML = "Hai inserito qualcosa che non è un numero!!!"
+    if (isNaN(num)) {
+        console.log("Hai inserito qualcosa che non è un numero!!!");
         i--
 
-    } else if (num1 > num2) {
+    } else {
 
-        document.querySelector(".num").innerHTML = num1;
+        num.push(userNum)
 
         i++
 
         console.log(num);
 
 
-    } else if (num2 > num1) {
-
-        document.querySelector(".num").innerHTML = num2;
-        i++
-
-    } else {
-        document.querySelector(".num").innerHTML = `I numeri sono uguali!`
-        i++
     }
 
 
+}
+
+if (num[0] > num[1]) {
+    alert(num[0])
+} else {
+    alert(num[1])
 }
